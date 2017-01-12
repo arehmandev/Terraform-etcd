14f3d2974817ac3efdb1ca04e901e5df5263f47d
arehmandev <arehmandev@outlook.com>
2017-01-12 20:22:35 +0000

    Workaround bug

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -4,7 +4,7 @@ output "master_profile_name" {
 }
 
 output "master_role_arn" {
-  value = "${aws_iam_role.master_role.arn}"
+  value = "${file("${path.module}/master_role_arn.txt")}"
 }
 
 output "worker_profile_name" {
@@ -12,5 +12,5 @@ output "worker_profile_name" {
 }
 
 output "worker_role_arn" {
-  value = "${aws_iam_role.worker_role.arn}"
+  value = "${file("${path.module}/worker_role_arn.txt")}"
 }
