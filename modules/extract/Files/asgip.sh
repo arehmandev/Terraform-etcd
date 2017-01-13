6a2e189f86fb050b884d0069ed6b18cb69a643b2
arehmandev <arehmandev@outlook.com>
2017-01-13 14:37:55 +0000

    Fixed bash script

--- modules/extract/Files/asgip.sh
+++ modules/extract/Files/asgip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 function getip {
 for i in `aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name $1 | grep -i instanceid  | awk '{ print $2}' | cut -d',' -f1| sed -e 's/"//g'`
 do
@@ -7,4 +8,4 @@ aws ec2 describe-instances --instance-ids $i | grep -i PrivateIpAddress | awk '{
 done;
 }
 
-getip | sed  's/^,//' | sed -e 's/^[ \t]*//'
+getip $1 | sed  's/^,//' | sed -e 's/^[ \t]*//'
